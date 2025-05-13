d4e5db83566e871c178b1dea569a6d83af2fb635
Frederick Mannings <fred.mannings@solvicode.com>
2025-05-13 10:28:58 +0100

    Network changes: - Added env vars to define the address the processor should attach to, and the PORT - Propagated these down to the gRPC service

--- orca_python/envs.py
+++ orca_python/envs.py
@@ -4,14 +4,22 @@ from typing import Tuple
 from orca_python.exceptions import MissingDependency
 
 
-def getenvs() -> Tuple[str, str]:
+def getenvs() -> Tuple[str, ...]:
+
     orcaserver = os.getenv("ORCASERVER", "")
     if orcaserver == "":
         MissingDependency("ORCASERVER is required")
+    orcaserver = orcaserver.lstrip("grpc://")
+
     port = os.getenv("PORT", "")
     if port == "":
         MissingDependency("PORT required")
-    return orcaserver, port
+
+    host = os.getenv("HOST", "")
+    if host == "":
+        MissingDependency("HOST is required")
+
+    return orcaserver, port, host
 
 
-ORCASERVER, PORT = getenvs()
+ORCASERVER, PORT, HOST = getenvs()
