cddb3d393940de5753bc6e4adc325cdc989b5bee
Frederick Mannings <fred.mannings@solvicode.com>
2025-06-23 20:34:59 +0100

    Modified the specification of windowType to be a class

--- examples/simpletrigger/processor.py
+++ examples/simpletrigger/processor.py
@@ -1,12 +1,16 @@
 import time
 from typing import Dict
 
-from orca_python import Processor
+from orca_python import Processor, WindowType
 
 proc = Processor("ml")
 
+Every30Second = WindowType(
+    name="Every30Second", version="1.0.0", description="Triggers every 30 seconds"
+)
 
-@proc.algorithm("MyAlgo", "1.0.0", "Every30Second", "1.0.0")
+
+@proc.algorithm("MyAlgo", "1.0.0", Every30Second)
 def my_algorithm() -> Dict[str, int]:
     time.sleep(5)
     return {"result": 42}
